6e52a4d3587230adf9d7f77432eb3a8f09bc3d64
Mohini5555 <69460250+Mohini5555@users.noreply.github.com>
2021-03-15 20:23:40 +0530

    Delete newfolder directory

--- newfolder/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';  
-import ReactDOM from 'react-dom';
-import ReactTable from "react-table";  
-import App from './App.js';  
-import DataTable from './App.js';
-  
-render(<DataTable />, document.getElementById('root'));
