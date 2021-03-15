f64cc8625898cb2256615e018281627f2838e3ca
Mohini5555 <69460250+Mohini5555@users.noreply.github.com>
2021-03-15 20:26:49 +0530

    Add files via upload

---
+++ React Table Component/main.js
@@ -0,0 +1,7 @@
+import React from 'react';  
+import ReactDOM from 'react-dom';
+import ReactTable from "react-table";  
+import App from './App.js';  
+import DataTable from './App.js';
+  
+render(<DataTable />, document.getElementById('root'));
